f28b3dd0907c6d266443be9948d4e2366afe246a
Barry Dempsey <barrydempsey.es@gmail.com>
2024-05-04 04:23:22 -0400

    Add code to find bt isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,10 +17,12 @@ public_users.get('/',function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    let filtered_books = allBooks.find((book) => book.isbn === isbn)
+    if (filtered_books == null) return res.status(300).json({message: "Book not found"});
+    res.status(300).json({message: filtered_books});
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
