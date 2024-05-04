a3450ff7411cedfdc34b800702dd17e5a77f0f3b
Barry Dempsey <barrydempsey.es@gmail.com>
2024-05-04 04:26:06 -0400

    Add code to get books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,9 +25,10 @@ public_users.get('/isbn/:isbn', function (req, res) {
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+    const authorName = req.params.author;
+    let filtered_books = allBooks.filter((book) => book.author.includes(authorName));
+    return res.status(300).json({message: filtered_books});
 });
 
 // Get all books based on title
